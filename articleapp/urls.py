5d0bdbc879f309f1a8a4ddba73813720a6627966
at-a-loss <942063433@qq.com>
2020-03-10 09:46:42 +0800

    文件管理

---
+++ articleapp/urls.py
@@ -0,0 +1,26 @@
+"""cmfz URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from article import views
+
+urlpatterns = [
+    path('getALLArticle/', views.getALLArticle),
+    path('upload_img/', views.upload_img),
+    path('kind/', views.kind),
+    path('get_all_img/', views.get_all_img),
+    path('add_article/', views.add_article),
+]
